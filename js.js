cfc7b2b6fb2ca6f48fd90683d9fd2f2fc286341a
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-03-14 21:18:25 +0500

    challenge two

--- js.js
+++ js.js
@@ -16,14 +16,14 @@
 //   <button>Click</button>
 // </div>
 
-const h1 = React.createElement("h1", null, "Hello Raza");
-const p = React.createElement("p", null, "Learning React");
-const btn = React.createElement("button", null, "Click");
-const div = React.createElement("div", null, [h1, p, btn]);
+// const h1 = React.createElement("h1", null, "Hello Raza");
+// const p = React.createElement("p", null, "Learning React");
+// const btn = React.createElement("button", null, "Click");
+// const div = React.createElement("div", null, [h1, p, btn]);
 
-const main = document.querySelector("#main");
-const root = ReactDOM.createRoot(main);
-root.render(div);
+// const main = document.querySelector("#main");
+// const root = ReactDOM.createRoot(main);
+// root.render(div);
 
 // <div>
 //   <h1>React Journey</h1>
@@ -34,3 +34,18 @@ root.render(div);
 //     <li>Rendering</li>
 //   </ul>
 // </div>
+
+const h1 = React.createElement("h1", null, "React Journey");
+const p = React.createElement("p", null, "Day 1");
+const ul = React.createElement("ul", null, [
+  React.createElement("li", null, "React Basics"),
+  React.createElement("li", null, "React.createElement"),
+  React.createElement("li", null, "Rendering"),
+]);
+
+
+const div = React.createElement("div", null, [h1, p, ul]);
+
+const main = document.querySelector("#main");
+const root = ReactDOM.createRoot(main);
+root.render(div);
